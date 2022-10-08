07823e7066a82c45bfda32e8641582020e180008
SantiagoCalderon1999 <santiagocalderon1999@gmail.com>
2022-10-07 21:37:10 -0500

    Include Table for symptoms

--- src/components/SymptomsForm.js
+++ src/components/SymptomsForm.js
@@ -7,6 +7,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import TableSymptoms from './TableSymptoms';
+import Grid from '@mui/material/Grid';
 
 /**
  * Creates a basic form
@@ -39,83 +40,90 @@ function BasicTextFields() {
         let tempTableRecords = tableRecords;
         tempTableRecords.push(inputs);
         setTableRecords(tempTableRecords);
-        console.log(tempTableRecords);
     };
     return (
-        <Box
-            component="form"
-            sx={{
-            '& .MuiTextField-root': { m: 1, width: '50ch' },
-            }}
-            noValidate
-            autoComplete="off"
+        <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
         >
-            <form>
-                <TextField 
-                    name="name"
-                    id="outlined-basic" 
-                    label="Name" 
-                    variant="outlined" 
-                    value={inputs.name}
-                    onChange={handleChange}
-                    type="text"
-                />
+            <Box
+                component="form"
+                sx={{
+                '& .MuiTextField-root': { m: 1, width: '80ch' },
+                }}
+                alignItems="center"
+                noValidate
+                autoComplete="off"
+            >
+                <form>
+                    <TextField 
+                        name="name"
+                        id="outlined-basic" 
+                        label="Name" 
+                        variant="outlined" 
+                        value={inputs.name}
+                        onChange={handleChange}
+                        type="text"
+                    />
+                    <br/>
+                    <TextField 
+                        name="email"
+                        id="outlined-basic" 
+                        label="Email" 
+                        variant="outlined" 
+                        value={inputs.email}
+                        onChange={handleChange}
+                        type="email"
+                    />
+                    <br/>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Birthdate"
+                        name="birthdate"
+                        value={inputs.birthdate}
+                        onChange={(newValue) => {
+                            setInputs((prevState) => ({
+                                ...prevState,
+                                birthdate: newValue.toString(),
+                            }));
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                    </LocalizationProvider>
+                    <br/>
+                    <TextField
+                        name="temperature"
+                        id="outlined-end-adornment"
+                        label="Temperature"
+                        variant="outlined" 
+                        sx={{ m: 1, width: '25ch' }}
+                        InputProps={{
+                            endAdornment: <InputAdornment position="end">°C</InputAdornment>,
+                        }}
+                        type="number"
+                        value={inputs.temperature}
+                        onChange={handleChange}
+                    />
+                    <br/>
+                    <TextField 
+                        name="symptoms"
+                        id="outlined-basic" 
+                        label="Symptom" 
+                        variant="outlined" 
+                        value={inputs.symptom}
+                        onChange={handleChange}
+                        type="text"
+                    />
+                    <br/>
+                    <Button type='submit' onClick={handleSubmit}>Submit</Button>
+                </form>
                 <br/>
-                <TextField 
-                    name="email"
-                    id="outlined-basic" 
-                    label="Email" 
-                    variant="outlined" 
-                    value={inputs.email}
-                    onChange={handleChange}
-                    type="text"
-                />
-                <br/>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                    label="Birthdate"
-                    name="birthdate"
-                    value={inputs.birthdate}
-                    onChange={(newValue) => {
-                        setInputs((prevState) => ({
-                            ...prevState,
-                            birthdate: newValue,
-                        }));
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-                </LocalizationProvider>
-                <br/>
-                <TextField
-                    name="temperature"
-                    id="outlined-end-adornment"
-                    label="Temperature"
-                    variant="outlined" 
-                    sx={{ m: 1, width: '25ch' }}
-                    InputProps={{
-                        endAdornment: <InputAdornment position="end">°C</InputAdornment>,
-                    }}
-                    type="number"
-                    value={inputs.temperature}
-                    onChange={handleChange}
-                />
-                <br/>
-                <TextField 
-                    name="symptoms"
-                    id="outlined-basic" 
-                    label="Symptom" 
-                    variant="outlined" 
-                    value={inputs.symptom}
-                    onChange={handleChange}
-                    type="text"
-                />
-                <br/>
-                <Button type='submit' onClick={handleSubmit}>Submit</Button>
-            </form>
-            <br/>
-            <TableSymptoms columnTitles={columnTitles}/>
-        </Box>
-        
+                <TableSymptoms columnTitles={columnTitles} rows={tableRecords}/>
+            </Box>
+        </Grid>
     );
 }
 
